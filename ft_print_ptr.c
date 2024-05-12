730620fadb3e767717fd0734948cd047ef6453a0
eesuhn <eason.yihong@gmail.com>
2024-05-13 01:47:01 +0800

    Fixed null pointer

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -14,13 +14,11 @@
 
 int	ft_print_ptr(va_list args)
 {
-	unsigned long long	ptr;
-	char				*hex;
-	int					len;
+	unsigned long	ptr;
+	char			*hex;
+	int				len;
 
-	ptr = va_arg(args, unsigned long long);
-	if (ptr == 0)
-		return (ft_putstr("(nil)"));
+	ptr = va_arg(args, unsigned long);
 	hex = ft_itoa_base(ptr, "0123456789abcdef");
 	len = ft_putstr("0x");
 	len += ft_putstr(hex);
